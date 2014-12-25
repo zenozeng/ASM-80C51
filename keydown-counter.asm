03780b546a5a8124e5a211004e22b996fc6b4542
Zeno Zeng <zenoofzeng@gmail.com>
2014-12-25 20:35:06 +0800

    add keydown-counter.asm

---
+++ keydown-counter.asm
@@ -0,0 +1,27 @@
+;;; keydown-counter.asm -- Count keydown form P3.2 and outputs in P1 using LED
+
+;; Author: Zeno Zeng <zenoofzeng@gmail.com>
+;; Time-stamp: <2014-12-25 20:34:21 Zeno Zeng>
+
+;;; Commentary:
+
+;; Count keydown from P3.2 then ouputs using 8bit P1 (in LED)
+
+;;; Code:
+
+	ORG 0000H
+        AJMP MAIN
+        ORG 0003H
+        AJMP ONKEYDOWN
+
+INIT:
+        CLR A
+        MOV TCON, #01H          ; 设置触发方式为脉冲，下降沿有效（IT0 = 1）
+        MOV IE, #81H            ; 中断总允许，允许 EX0（P3.2 引入） 外中断 （EA = 1, EX0 = 1）
+
+LISTENING:
+        SJMP LISTENING
+
+ONKEYDOWN:
+        INC A
+        MOV P1, A
